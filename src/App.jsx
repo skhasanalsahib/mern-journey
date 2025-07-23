05abc55b240cd75227ba681fc8c5522933fd0c8e
Sk Hasan Al Sahib <skhasanalsahib@gmail.com>
2025-07-23 16:39:10 +0600

    Learn to use useMemo, React.memo, useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { useCallback, useMemo, useState } from "react";
+import Button from "./components/Button";
+import ShowCount from "./components/ShowCount";
+import Title from "./components/Title";
+
 function App() {
-  return <>Hello</>;
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const incrementByOne = useCallback(() => {
+    setCount1((prevCount) => prevCount + 1);
+  }, []);
+
+  const incrementByFive = useCallback(() => {
+    setCount2((prevCount) => prevCount + 5);
+  }, []);
+
+  const expensiveValue = useMemo(() => {
+    let xValue = 0;
+    for (let i = 0; i <= 1000000000; i++) {
+      xValue += i;
+    }
+    return xValue;
+  }, []);
+
+  console.log(expensiveValue);
+
+  return (
+    <div className="app">
+      <Title />
+      <ShowCount count={count1} title="Counter 1" />
+      <Button handleClick={incrementByOne}>Increment by one</Button>
+      <hr />
+      <ShowCount count={count2} title="Counter 2" />
+      <Button handleClick={incrementByFive}>Increment by five</Button>
+    </div>
+  );
 }
 
 export default App;
